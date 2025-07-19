ad70e6ca382a47becc7ee098266d04a57bdb9ef7
Juan Santamaria <juandavidsantamariabeltran123@gmail.com>
2025-07-19 15:21:40 -0500

    solve 49 group anagrams

--- 49_group_anagrams/v3.rb
+++ 49_group_anagrams/v3.rb
@@ -1,9 +1,9 @@
 
 # @param {String[]} strs
 # @return {String[][]}
-#Runtime 38 ms (Beats 70.16%), Memory 217.48 MB (Beats 50.79%)
+# BEST
 def group_anagrams(strs)
-    #Convertimos el str a su representacion en ASCII(bytes) luego contamos la frecuencia y finalmente hacemos un hash (huella de una estructura de datos ) y si la hulla es igual lo agrupa 
+    #Convertimos el str a su representacion en ASCII(bytes) luego contamos la frecuencia(tally) y finalmente hacemos un hash (huella de una estructura de datos no reversible) y los agrupa por huellas
     strs.group_by {|str| str.bytes.tally.hash}.values
 end
 
